4a630cae748cf5d4c6edd7198bad499f3eca6cc1
ilyashvts <ilya.shvts919@gmail.com>
2022-07-08 12:20:03 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://tensionx.herokuapp.com',
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   await app.listen(3000);
