125da6f5de2619587ac3ffd2e000a8fc9e733570
ilyashvts <ilya.shvts919@gmail.com>
2022-07-08 12:31:51 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: false,
   });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
